2ed886d540d19f44abdbdf0b91ecab0c07feb0a3
rajneeshrana0 <ranarajneesh038@gmail.com>
2024-08-02 18:09:12 +0530

    few bugs Fixies

--- app/page.tsx
+++ app/page.tsx
@@ -105,7 +105,7 @@ export default function Home() {
         <div className="mx-auto mt-16 max-w-7xl px-6 sm:mt-20 md:mt-24 lg:px-8">
           <dl className="mx-auto grid max-w-2xl grid-cols-1 gap-x-6 gap-y-10 text-base leading-7 text-gray-700 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3 lg:gap-x-8 lg:gap-y-16">
             {features.map((feature) => (
-              <div className="relative pl-9">
+              <div key={feature.name} className="relative pl-9">
                 <dt className="inline font-semibold text-gray-900">
                   <feature.icon className="absolute left-1 top-1 h-5 w-5 text-indigo-600" />
                 </dt>
